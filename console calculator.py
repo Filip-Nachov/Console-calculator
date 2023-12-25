de733627a2db2ed8d3c7a150bdca4650b978d84e
Filip-Nachov <137638778+Filip-Nachov@users.noreply.github.com>
2023-12-25 06:54:55 +0100

    Add files via upload

---
+++ console calculator.py
@@ -0,0 +1,47 @@
+def add(x, y):
+    return x + y
+
+def subtract(x, y):
+    return x - y
+
+def multiply(x, y):
+    return x * y
+
+def divide(x, y):
+    return x / y
+
+print("select operation")
+print("1.add")
+print("2.subtract")
+print("3.multiply")
+print("4.divide")
+
+while True:
+    choice = input("select 1/2/3/4 = ")
+
+    if choice in ('1', '2', '3', '4'):
+        try:
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+            continue
+
+    if choice == '1':
+        print(num1, "+", num2, "=", add(num1, num2))
+
+    elif choice == '2':
+        print(num1, "-", num2, "=", subtract(num1, num2))
+
+    elif choice == '3':
+        print(num1, "*", num2, "=", multiply(num1, num2))
+
+    elif choice == '4':
+        print(num1, "/", num2, "=", divide(num1, num2))
+
+
+    next_calculation = input("Let's do next calculation? (yes/no): ")
+    if  next_calculation == "no":
+        break
+    else:
+        print("Invalid Input")
